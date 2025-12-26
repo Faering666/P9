58993c3decedb4d0a222f6cd1d4b66dfde9b54d3
Tosuma <tobias@surland.dk>
2025-12-26 14:44:21 +0100

    Updated training runner for new training

--- runner.sh
+++ runner.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
+
+# ---------- 1st run ----------
 # Base run, with transferlearning on WeedyRice
-python src/tl-pipeline.py \
- --stage1_data_path data/East-Kaza \
- --stage1_data_type Kazakhstan \
- --stage1_epochs 300 \
- --stage1_lr 4e-4 \
- --stage2_data_path data/WeedyRice \
- --stage2_data_type Weedy-Rice \
- --stage2_epochs 100 \
- --stage2_lr 1e-5 \
- --stage3_data_path data/WeedyRice \
- --stage3_data_type Weedy-Rice \
- --stage3_epochs 100 \
- --stage3_lr 1e-7 
+# python src/tl-pipeline.py \
+#  --stage1_data_path data/East-Kaza \
+#  --stage1_data_type Kazakhstan \
+#  --stage1_epochs 300 \
+#  --stage1_lr 4e-4 \
+#  --stage2_data_path data/WeedyRice \
+#  --stage2_data_type Weedy-Rice \
+#  --stage2_epochs 100 \
+#  --stage2_lr 1e-5 \
+#  --stage3_data_path data/WeedyRice \
+#  --stage3_data_type Weedy-Rice \
+#  --stage3_epochs 100 \
+#  --stage3_lr 1e-7 
 
-mkdir checkpoints/basemodel-tl-Weed
-mv checkpoints/s* checkpoints/basemodel-tl-Weed
+# mkdir checkpoints/basemodel-tl-Weed
+# mv checkpoints/s* checkpoints/basemodel-tl-Weed
 
-#Using base model, skipping stage2, transferlearning on sri
+# ---------- 2nd run ----------
+# Using base model, skipping stage2, transferlearning on sri
 python src/tl-pipeline.py \
  --stage1_data_path data/East-Kaza \
  --stage1_data_type Kazakhstan \
@@ -37,7 +40,8 @@ mkdir checkpoints/Sri-Lanka-stage3-only
 mv checkpoints/s* checkpoints/Sri-Lanka-stage3-only
 
 
-#Using base model, transferlearning on aligned-sri-lanka
+# ---------- 3rd run ----------
+# Using base model, transferlearning on aligned-sri-lanka
 python src/tl-pipeline.py \
  --stage1_data_path data/East-Kaza \
  --stage1_data_type Kazakhstan \
@@ -56,26 +60,28 @@ python src/tl-pipeline.py \
  mkdir checkpoints/tl-sri-lanka
  mv checkpoints/s* checkpoints/tl-sri-lanka
 
-#Using base model, skipping stage 2, transferlearning on WeedyRice
-python src/tl-pipeline.py \
- --stage1_data_path data/East-Kaza \
- --stage1_data_type Kazakhstan \
- --stage1_epochs 0 \
- --stage1_lr 4e-4 \
- --stage2_data_path data/WeedyRice \
- --stage2_data_type Weedy-Rice \
- --stage2_epochs 0 \
- --stage2_lr 1e-5 \
- --stage3_data_path data/WeedyRice \
- --stage3_model checkpoints/basemodel-tl-Weed/stage1_best_final.pth \
- --stage3_data_type Weedy-Rice \
- --stage3_epochs 100 \
- --stage3_lr 1e-7 
+# ---------- 4th run ----------
+# Using base model, skipping stage 2, transferlearning on WeedyRice
+# python src/tl-pipeline.py \
+#  --stage1_data_path data/East-Kaza \
+#  --stage1_data_type Kazakhstan \
+#  --stage1_epochs 0 \
+#  --stage1_lr 4e-4 \
+#  --stage2_data_path data/WeedyRice \
+#  --stage2_data_type Weedy-Rice \
+#  --stage2_epochs 0 \
+#  --stage2_lr 1e-5 \
+#  --stage3_data_path data/WeedyRice \
+#  --stage3_model checkpoints/basemodel-tl-Weed/stage1_best_final.pth \
+#  --stage3_data_type Weedy-Rice \
+#  --stage3_epochs 100 \
+#  --stage3_lr 1e-7 
 
-mkdir checkpoints/weed-rice-stage3-only
-mv checkpoints/s* checkpoints/weed-rice-stage3-only
+# mkdir checkpoints/weed-rice-stage3-only
+# mv checkpoints/s* checkpoints/weed-rice-stage3-only
 
-#Using stage3 model, transferlearning on Sri-lanka
+# ---------- 5th run ----------
+# Using stage3 model, transferlearning on Sri-lanka
 python src/tl-pipeline.py \
  --stage1_data_path data/East-Kaza \
  --stage1_data_type Kazakhstan \
@@ -94,7 +100,9 @@ python src/tl-pipeline.py \
 mkdir checkpoints/Sri-lanka-stage2-trained-on-stage3
 mv checkpoints/s* checkpoints/Sri-lanka-stage2-trained-on-stage3
 
-#Using stage3 model, transferlearning on WeedyRice
+# ---------- 6th run ----------
+# THIS SHOULD BE RUN AS WELL! There was an error in the pathing to the correct model, so it has to be run again
+# Using stage3 model, transferlearning on WeedyRice
 python src/tl-pipeline.py \
  --stage1_data_path data/East-Kaza \
  --stage1_data_type Kazakhstan \
@@ -102,7 +110,7 @@ python src/tl-pipeline.py \
  --stage1_lr 4e-4 \
  --stage2_data_path data/WeedyRice \
  --stage2_data_type Weedy-Rice \
- --stage2_model checkpoints/weed-rice-stage3/stage3_best_final.pth \
+ --stage2_model checkpoints/weed-rice-stage3-only/stage3_best_final.pth \
  --stage2_epochs 100 \
  --stage2_lr 1e-5 \
  --stage3_data_path data/WeedyRice \
@@ -111,4 +119,4 @@ python src/tl-pipeline.py \
  --stage3_lr 1e-7 
 
 mkdir checkpoints/Weedy-Rice-stage2-trained-on-stage3
-mv checkpoints/s* checkpoints/Sri-lanka-stage2-trained-on-stage3
+mv checkpoints/s* checkpoints/Weedy-Rice-stage2-trained-on-stage3
