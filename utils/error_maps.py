e9d8e1b9636ddcb672370a1ed8aca1a2bb9a7bd1
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2026-01-03 11:43:29 +0100

    Add error maps and update error maps script

--- utils/error_maps.py
+++ utils/error_maps.py
@@ -2,6 +2,7 @@ import argparse
 import cv2
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 import os
 import sys
@@ -24,8 +25,8 @@ def find_image_files(rgb_path):
 
 def load_image_as_array(path: str, resize: bool = False, resize_size: tuple = (512, 480)):
     img = Image.open(path)
+    img = np.array(img)
     if resize:
-        img = np.array(img)
         img = cv2.resize(img, resize_size, interpolation=cv2.INTER_CUBIC)
     return img.astype(np.float32)
 
@@ -33,11 +34,22 @@ def save_array_as_image(array, path):
     img = Image.fromarray(np.clip(array, 0, 255).astype(np.uint8))
     img.save(path)
 
+def save_array_as_pink_colormap(array, path):
+    # Normalize array to 0-1
+    arr_norm = np.clip(array, 0, 1)
+    # Use matplotlib's 'pink' colormap
+    cmap = plt.get_cmap('pink')
+    arr_colored = cmap(arr_norm)
+    # Convert to 8-bit RGB
+    arr_rgb = (arr_colored[:, :, :3] * 255).astype(np.uint8)
+    img = Image.fromarray(arr_rgb)
+    img.save(path)
+
 def ndvi_error_map(pred, gt):
-    pred_nir = load_image_as_array(pred_files["nir"], True, (1280, 720))
-    pred_r = load_image_as_array(pred_files["r"], True, (1280, 720))
-    gt_nir = load_image_as_array(gt_files["nir"], True, (1280, 720))
-    gt_r = load_image_as_array(gt_files["r"], True, (1280, 720))
+    pred_nir = load_image_as_array(pred_files["nir"])
+    pred_r = load_image_as_array(pred_files["r"])
+    gt_nir = load_image_as_array(gt_files["nir"])
+    gt_r = load_image_as_array(gt_files["r"])
     
     pred = (pred_nir - pred_r) / (pred_nir + pred_r + 1e-6)
     gt = (gt_nir - gt_r) / (gt_nir + gt_r + 1e-6)            
@@ -60,27 +72,15 @@ if __name__ == "__main__":
 
     # These are the path to the predicted and ground truth of the rgb images.
     # Expects _re, _g, _r, _nir suffix
-    parser.add_argument("--pred_path", default="data/pred.JPG")
-    parser.add_argument("--gt_path", default="data/ground_truth.TIF")
-    parser.add_argument("--error_map_type", default="ndvi", choices=["all", "ndvi", "ndre"])
+    parser.add_argument("--pred_path", default="data/error_maps/pred.JPG")
+    parser.add_argument("--gt_path", default="data/error_maps/gt.TIF")
     args = parser.parse_args()
 
     pred_files = find_image_files(args.pred_path)
     gt_files = find_image_files(args.gt_path)
 
-    match args.error_map_type:
-        case "ndvi":
-            ndvi_error_map = ndvi_error_map(pred_files, gt_files)
-            save_array_as_image(ndvi_error_map * 255, "ndvi_error_map.png")
-        case "ndre":
-            ndre_error_map = ndre_error_map(pred_files, gt_files)
-            save_array_as_image(ndre_error_map * 255, "ndre_error_map.png")
-        case "all":
-            # ndvi
-            ndvi_error_map = ndvi_error_map(pred_files, gt_files)
-            save_array_as_image(ndvi_error_map * 255, "ndvi_error_map.png")
-            
-            # ndre
-            ndre_error_map = ndre_error_map(pred_files, gt_files)
-            save_array_as_image(ndre_error_map * 255, "ndre_error_map.png")
-    
+    ndvi_error = ndvi_error_map(pred_files, gt_files)
+    save_array_as_pink_colormap(ndvi_error, "ndvi_error_map_pink.png")
+
+    ndre_error = ndre_error_map(pred_files, gt_files)
+    save_array_as_pink_colormap(ndre_error, "ndre_error_map_pink.png")
